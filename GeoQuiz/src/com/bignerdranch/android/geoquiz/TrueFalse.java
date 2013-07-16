d7e5021c07445d8c815234dbd19700d912196edd
Héctor Pablos <hctpbl@gmail.com>
2013-07-16 17:21:28 +0200

    Commit inicial

---
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.geoquiz;
+
+public class TrueFalse {
+	
+	private int mQuestion;
+	
+	private boolean mTrueQuestion;
+	
+	public TrueFalse(int question, boolean trueQuestion) {
+		mQuestion = question;
+		mTrueQuestion = trueQuestion;
+	}
+
+}
